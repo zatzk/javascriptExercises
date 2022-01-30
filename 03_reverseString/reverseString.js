c78066a4597757738aeaf0a6862e4f913f844258
zatzk <zatzk@outlook.com>
2022-01-29 22:17:25 -0300

    question 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
 const reverseString = function(str) {
-
+  var splitString = str.split('');
+  var reverseArray = splitString.reverse();
+  var joinArray = reverseArray.join('');
+  return joinArray;
 };
 
 // Do not edit below this line
