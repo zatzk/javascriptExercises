89d7c8865db214f9f551c8a870db4639f2f990d8
zatzk <zatzk@outlook.com>
2022-02-13 14:30:14 -0300

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let sum = 0;
+  if(num1 > 0 && num1 < num2) {
+    for(i = num1; i <= num2; i++) {
+      sum += i;
+    }
+  }else if(num2 > 0 && num1 > num2){
+    for(i = num2; i <= num1; i++) {
+      sum += i;
+    }
+  }else {
+    sum = 'ERROR';
+  }
+  return sum;
 };
 
 // Do not edit below this line
