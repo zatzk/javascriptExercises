576a71da90bb1032606ebe5bd56df8ecbeb0006b
zatzk <zatzk@outlook.com>
2022-02-15 17:17:22 -0300

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes =  (str) => {
+  let newStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let reverseStr = newStr.split('').reverse('').join('');
+  return true ? reverseStr == newStr : false;
 };
 
 // Do not edit below this line
