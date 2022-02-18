a0bef97e6ca42e96b90a1c90f77d7962b9cccfa2
zatzk <zatzk@outlook.com>
2022-02-18 15:11:43 -0300

    findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = (people) => {
+  
+  return people.reduce((prev, curr) => {
+    const prevAge = getAge(prev.yearOfBirth, prev.yearOfDeath);
+    const currAge = getAge(curr.yearOfBirth, curr.yearOfDeath);
+    return prevAge < currAge ? curr : prev;
+  })
+};
 
+const getAge = (birth, death) => {
+  if(!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
