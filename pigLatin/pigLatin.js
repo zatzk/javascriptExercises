4b8a9b8def538f99dea77290827e9f1d9daf8114
zatzk <zatzk@outlook.com>
2022-02-22 10:50:30 -0300

    pigLatin

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,5 +1,23 @@
 function pigLatin(string) {
-  
+  return string.split(' ').map(word => {
+    const vowels = /[aeio]/;
+    const consonants = /[bcdfghjklmnpqrstvuwxyz]/
+    
+    
+    if (word[0].match(consonants) && word[1].match(consonants) && word[2].match(consonants)) {
+      return word.substring(3) + word[0] + word[1] + word[2] + 'ay';
+    } 
+    if (word[0].match(consonants) && word[1].match(consonants)) {
+      return word.substring(2) + word[0] + word[1] + 'ay';
+    } 
+    if (word[0].match(vowels)) {
+      return word + 'ay';
+    } 
+    if (word[0].match(consonants)) {
+      return word.substring(1) + word[0] + 'ay';
+    } 
+    
+  }).join(' ');
 };
   
 // Do not edit below this line
